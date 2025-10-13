f64bf4198bb910e90e7de1554feebb1cb46c7b8a
Matteo <matteo.mb29@gmail.com>
2025-10-13 19:17:08 +0200

    Aggiornamento 13ottobre

--- imparando/MOD2/GPU/MatrixMultiplication_global.h
+++ imparando/MOD2/GPU/MatrixMultiplication_global.h
@@ -1,12 +1,46 @@
+//questo programma è scritto in cuda c, estensione del c creato da nvidia
+
 #include <stdio.h>
+
 // Matrices are stored in row-major order:
 // M(row, col) = *(M.elements + row * M.width + col)
+
+
+//definisco una struttura matrice con attributi 
+//larghezza, altezza, elementi
+
 typedef struct {
+
 int width;
 int height;
-float* elements;
+float* elements; //puntatore a elements
+//che sarà un array di valori di tipo float, ricordo che in C/C++ 
+//posso usare un puntatore per indicare un array non ancora inizializzato
+
 } Matrix;
+
+//dato personalizzato, in cui raggruppo degli attributi,
+//a differenza delle classi in python qui non ho metodi, non può contenere azioni
+
+
+//una sintassi equivalente sarebbe 
+//// Stile C++ (più comune)
+//struct Matrix {
+   // int width;
+    //int height;
+    //float* elements;
+//};
+
+
 
 // Thread block size
 #define BLOCK_SIZE 16
+//serve per definire delle costanti come se fossero private
+
+
+//ricordo che __global__significa che la funzione MatMulKernel è un kernel
 __global__ void MatMulKernel(const Matrix, const Matrix, Matrix);
+
+//i primi due argomenti sono le matrici di input che definisco const cosi che la funzione
+//MatMulKernel non può cambiarle, questo è necessario per sicurezza del codice e per 
+//velocizzare l' accesso alla memoria da parte del compilatore (soprattutto per quest' ultimo motivo)
--- imparando/MOD2/GPU/esempio.cpp
+++ imparando/MOD2/GPU/esempio.cpp
@@ -1,5 +1,11 @@
-#include <iostream>
 
+//con questo programma vogliamo fare chiarezza sull' utilizzo di array 
+// e puntatori in c++, esplicando analogie e differenze
+// in C/C++ puoi usare i puntatori per definire vettori non inizializzati 
+//cioè blocchi di memoria che decidi tu di allocare in seguito
+
+
+#include <iostream>
 using namespace std;
 
 int main(){
@@ -12,8 +18,40 @@ int main(){
 
 
 
+    int array[] = {10, 20, 30};
+    int * puntatore_array = array;
+    cout << puntatore_array << endl;
+    cout << &puntatore_array <<endl;    
+    cout << array[1] << endl;
+    cout << puntatore_array[1]<< endl;
+    cout << "Indirizzo del primo numero: " << static_cast<void*>(puntatore_array) << endl;
 
+
+    int* puntatore_primo_elemento = &array[1];
+    cout << puntatore_primo_elemento<<endl;
+
+
+
+
+    char* parola = "ciao";
+    cout << parola <<endl;
+
+    char parola2[] = "ciao";
+    char* puntatore_parola = parola2; 
+    cout << "Indirizzo del primo carattere: " << static_cast<void*>(puntatore_parola) << endl;
+    cout << puntatore_parola << endl;
+
+    char bestemmia[] = "dio bastardo";
+    char* blasfemia = "dio bastardo";
+
+    for (int i = 0; i < 3; i++){
+        cout << bestemmia[i] << endl;
+    }
+
+    char * bestemmie[] = {"dio porcone", "dio cane", "dio infame"};
+    cout << bestemmie[1] << endl;
 
     return 0;
+
 
 }
