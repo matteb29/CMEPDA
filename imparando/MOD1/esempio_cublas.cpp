166ddeceb132901b74f91ecb92e728aca4a0b258
Matteo <matteo.mb29@gmail.com>
2025-10-15 12:06:10 +0200

    Aggiornamento 15 ottobre

---
+++ imparando/MOD1/esempio_cublas.cpp
@@ -0,0 +1,27 @@
+#include <cublas_v2.h>
+#include <cuda_runtime.h>
+#include <iostream>
+
+
+int a = 42;
+int n = 10;
+float x[n], y[n];
+
+// fill x, y
+cublasInit();
+
+float * d_x, * d_y;
+
+cudaMalloc((void **)&d_x, n * sizeof(x[0]);
+
+cudaMalloc((void **)&d_y, n * sizeof(y[0]);
+
+cublasSetVector(n, sizeof(x[0]), x, 1, d_x, 1);
+
+cublasSetVector(n, sizeof(y[0]), y, 1, d_y, 1);
+
+cublasSaxpy(n, a, d_x, 1, d_y, 1);
+
+cublasGetVector(n, sizeof(y[0]), d_y, 1, y, 1);
+
+cublasShutdown();
