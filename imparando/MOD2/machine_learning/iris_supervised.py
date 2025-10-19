00e59230a6e661b4397fb92909dc817306c764b5
Matteo <matteo.mb29@gmail.com>
2025-10-19 17:35:43 +0200

    Aggiornamento 19 ottobre

--- imparando/MOD2/machine_learning/iris_supervised.py
+++ imparando/MOD2/machine_learning/iris_supervised.py
@@ -10,8 +10,9 @@
 import seaborn as sns #importiamo la libreria per il scaricare il dataset degli iris
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-
+from sklearn.model_selection import train_test_split #per splittare il dataset in training e valutazione
+from sklearn.naive_bayes import GaussianNB #per importare lo stimatore
+from sklearn.metrics import accuracy_score #per quantizzare precisione del modello
 
 
 iris = sns.load_dataset("iris")
@@ -45,7 +46,7 @@ plt.show()
 
 #X_iris = iris.drop('species', axis=1)
 
-print(iris_np[:10]) #first 10 lines
+#print(iris_np[:10]) #first 10 lines
 
 X_iris=iris_np[:,0:-1] #-1 indica l' ultimo elemento escluso,
 # : -> prendi tutte le righe
@@ -55,15 +56,15 @@ X_iris=iris_np[:,0:-1] #-1 indica l' ultimo elemento escluso,
 
 print(X_iris.shape)
 
-print(X_iris[:10]) #first 10 lines
+#print(X_iris[:10]) #first 10 lines
 
 
 #questo sarà il vettore target: cioè l' ultima colonna del dataframe iris,
 #ovvero la specie dei fiori
 y_iris = iris_np[:, -1]
 
-print(y_iris.shape)
-print(y_iris)
+#print(y_iris.shape)
+#print(y_iris)
 
 #print(y_iris[:10]) se volessi stampare le prime 10 
 
@@ -99,3 +100,37 @@ X_train, X_test, y_train, y_test = train_test_split(X_iris, y_iris, random_state
 #assicura che ogni volta che lancio il codice i dati siano suddivisi nel solito modo
 #  tra parte di training e parte di test: possiamo vedere random_state = 1 come generatore pseudocasuale
 
+
+
+model = GaussianNB() #creo una istanza dell' oggetto GaussianNB: uno stimatore
+
+model.fit(X_train, y_train) #chiamo il metodo .fit() dell' oggetto model
+#per allenare il mio modello, .fit(features, etichette)
+#model.fit fa un fit per ogni feature di ogni specie utilizzando una funzione gaussiana per ogni fit. Restituisce per
+#ogni specie la media e la deviazione standard di ogni feature della specie: sa disegnare la pdf gaussiana di ogni caratteristica 
+#per ogni specie: può lavorare con 12 gaussiane diverse
+#alla fine di questo processo ha info su tutte e 3 le specie, può iniziare a fare classificazione
+
+
+y_model = model.predict(X_test)  #definisco le y predette dal modello chiamando
+#il metodo .predict dell' oggetto model, le y che dovrà predire saranno quelle associate
+#ai dati X_test    .predict(features delle etichette da predire)
+#per ogni feature del dato di test lui confronta il valore della feature con la gaussiana corrispondente, assegnando
+#un punteggio sulla base di quanto sia probabile che il dato appartenga a quella gaussiana: il valore della pdf in quel punto
+
+
+
+print(np.stack((y_model, y_test), axis = 1))  #qui creo una tabella di confronto tra le etichette di test
+#e le etichette predette dal mio modello, axis = 1 dice prendi i due array y_model e y_test e mettili uno affianco all' altro
+#se avessi messo axis = 0 li avrebe messi uno sotto l' altro (axis = 0 oppure 1)
+
+
+print(y_model != y_test) #questo stampa un array di False, True. False se il modello ha azzeccato e quindi
+#non è vero che y_model è diverso da y_test; True se il modello ha sbagliato cioè se è vero che y_test è diverso da y_model
+
+print(sum(y_model == y_test)/len(y_model)) #così stampo quantre volte il modello ha azzeccato 
+#sum(y_model == ytest) conta il numero totale di True, che corrisponde al numero totale di previsioni corrette.
+
+
+#accuracy_score serve per capire quanto è stato accurato il modello
+print(accuracy_score(y_test, y_model))
